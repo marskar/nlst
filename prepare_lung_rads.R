dcc7582fbe443f59438f86049e82e0f1949aa429
Martin Skarzynski <marskar@gmail.com>
2019-02-26 16:29:48 -0500

    write R script for preparing lungrads data

---
+++ prepare_lung_rads.R
@@ -0,0 +1,16 @@
+#### Load packages ####
+lapply(c('here', 'haven', 'dplyr', 'purrr'), require, character.only = TRUE)
+
+# Define function for reshaping lungrads data
+lrads_extract <- function(df, x) {
+    df %>% select(c(pid, LRcat = paste0('slungrad', x))) %>% mutate("interval" = rep(x, nrow(.)))
+}
+
+# Load lung-RADS dataset https://biometry.nci.nih.gov/plcosub/lung/2009-00516-201204-0017-lung-mortality-risk/nlst/lungrads1.sas7bdat/@@download/file/lungrads1.sas7bdat from https://biometry.nci.nih.gov/plcosub/lung/2009-00516-201204-0017-lung-mortality-risk/nlst/lungrads1.sas7bdat/view
+# Use above function to reshape lungrads data
+# Save RDS file
+haven::read_sas(here("lungrads1.sas7bdat")) %>% 
+    map_df(0:2, lrads_extract, df=.) %>% 
+    saveRDS(file='lungrads.rds')
+
+readRDS('lungrads.rds')
