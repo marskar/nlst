5fa2b8184fd260bb3a14284b662b2105101e1c1e
Martin Skarzynski <marskar@gmail.com>
2019-04-05 08:25:33 -0400

    fix variables names

--- R/positives.R
+++ R/positives.R
@@ -226,7 +226,7 @@ data.screen$pkyears.cat <- factor(data.screen$pkyears.cat, levels=1:3, labels=c(
 abn <- readRDS(here("data/abn_lrads_merged.rds"))
 data.screen.abn <- merge(data.screen, abn, by=c("pid","interval"), all.x=TRUE, all.y=FALSE)
 # Replace NAs with 0 (not present) for appropriate variables
-replacevars  <- names(abn.pl.all)[!names(abn.pl.all) %in% c("pid","interval","LRcat","LRcatcol.neg","LRcatcol.pos")]
+replacevars  <- names(abn)[!names(abn) %in% c("pid","interval","LRcat","LRcatcol.neg","LRcatcol.pos")]
 data.screen.abn[replacevars][is.na(data.screen.abn[replacevars])]  <- 0
 data.screen.abn <- mutate(data.screen.abn, longest.diam = ifelse(is.infinite(longest.diam), 0, longest.diam))
 # Make a variable for including observations in Lung-RADS analysis
@@ -253,11 +253,11 @@ data.screen.abn.pos <- mutate(data.screen.abn.pos, growth.3l =
                           1*(interval==1) + 2*(interval==2 & any.growth==0) + 3*(interval==2 & any.growth==1))
 data.screen.abn.pos$growth.3l <- factor(data.screen.abn.pos$growth.3l, levels = c(1,2,3), labels=c("NA","No","Yes"))
 # Will need this vector for exploratory analysis of abnormalities (24 Jan 2017 - also create interaction vectors)
-abnlist <- abnlist.pos <- c(names(abn.pl.all)[5:32], "diam.cat") # omits longest.diam and any.nodule, but includes diam.cat which subsumes both. 12 Oct 2018 now includes any.new.nodule
+abnlist <- abnlist.pos <- c(names(abn)[5:32], "diam.cat") # omits longest.diam and any.nodule, but includes diam.cat which subsumes both. 12 Oct 2018 now includes any.new.nodule
 abnlist.pos.int <- lapply(abnlist.pos, function(x) {substitute(logit1yrisk:i, list(i=as.name(x)))})
 
 # Make dataset of unique individuals for descriptive table of false-positives
-all.subj.pos <- dplyr::filter(nlst.CT, pid %in% data.screen.abn.pos$pid)
+all.subj.pos <- dplyr::filter(nlst_CT, pid %in% data.screen.abn.pos$pid)
 all.subj.pos <- mutate(all.subj.pos, age.cat=as.factor(ifelse(age>=55 & age<60, "55-59", ifelse(age>=60 & age<65, "60-64",
                           ifelse(age>=65 & age<70, "65-69", ifelse(age>=70 & age<75, "70-74", NA))))),
                        qtyears.cat=as.factor(ifelse(qtyears==0, "Current smoker", ifelse(qtyears>0 & qtyears<=5, "1-5",
@@ -278,7 +278,6 @@ glm.screen.pos.abn.log <- glm(case ~ log1yrisk:diam.cat + log1yrisk:any.growth +
                                 log1yrisk:any.spiculation + log1yrisk:I(any.poor.def==1|any.margin.unab==1) -1, 
                                 data=data.screen.abn.pos, family=binomial(link='log'))
 data.screen.abn.pos$post.risk.abn.pos <- fitted.values(glm.screen.pos.abn.log)
-
 
 
 # --------------------------- run code to this line for data setup ----------------------------- #
