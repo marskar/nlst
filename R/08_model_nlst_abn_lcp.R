faaa535fc3c6a07001646c4decd439fbc20c2666
Martin Skarzynski <marskar@gmail.com>
2019-10-08 07:36:10 -0400

    remove dup code in script 8

--- R/08_model_nlst_abn_lcp.R
+++ R/08_model_nlst_abn_lcp.R
@@ -8,16 +8,3 @@ nlst_abn_lcp$case <- as.factor(nlst_abn_lcp$case)
 aml <- h2o.automl(y = "case", training_frame = nlst_abn_lcp)
 
 lb <- aml@leaderboard
-
-lag_vars <- function(.data, .group_var, .vars) {
-    .data %>%
-        group_by(!!enquo(.group_var)) %>%
-        mutate_at(.vars, .funs = list(lag = ~ lag))
-}
-
-colname_vector <- names(nlst_abn)[35:68]
-nlst_abn %>%
-    lag_vars(pid, colname_vector) %>%
-    lag_vars(pid, paste0(colname_vector, "_lag")) %>%
-    saveRDS(file = "data/nlst_abn_lag.rds")
-names(nlst_abn)
