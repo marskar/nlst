8a52efda188835a931f717c568915adf667ebaa6
Martin Skarzynski <marskar@gmail.com>
2020-01-14 16:30:18 -0500

    get predictions from models fit to entire dataset

--- R/06_plot_nlst_abn_lcp_pre.R
+++ R/06_plot_nlst_abn_lcp_pre.R
@@ -2,7 +2,10 @@
 library(readr)
 library(here)
 library(dplyr)
+library(leaps)
+library(stringr)
 library(rsample)
+library(broom)
 library(recipes)
 library(parsnip)
 # remotes::install_github("tidymodels/tune")
@@ -15,19 +18,107 @@ data <-
     read_rds(here("data/nlst_abn_lcp_pre.rds")) %>%
     mutate(case_at_next_screen = as.factor(case_at_next_screen))
 
+# Formulas ----
+lcp_form <- as.formula(case_at_next_screen ~ max_lcp_score)
 
-# Model settings ----
+lcp_lcrat_form <- as.formula(case_at_next_screen ~ max_lcp_score + logit1yrisk)
+
+bss_data <-
+    read_rds(here("data/nlst_abn_lcp_pre.rds")) %>%
+    select(
+        pid,
+        case_at_next_screen,
+        logit1yrisk,
+        diam_cat,
+        any_growth,
+        emphysema,
+        consolidation,
+        adenopathy,
+        any_upper,
+        any_right_mid,
+        any_lingula,
+        any_mixed,
+        any_spiculation,
+        any_poor_def,
+        any_margin_unab
+           )
+
+names(bss_data)
+# Model selection decides whether variable is a main effect or interaction
+
+get_best_bic_formula <- function(form, data, yname) {
+    regsumm <-
+        summary(leaps::regsubsets(
+            x = form,
+            data = bss_data,
+            nvmax = 50,
+            method = "exhaustive"
+        ))
+    coefs <- regsumm$which[which.min(regsumm$bic), -1]
+    predictors <- names(which(coefs == TRUE)) %>%
+        str_remove("\\d$") %>%
+        unique() %>%
+        paste(collapse = "+")
+    as.formula(paste(yname, "~", predictors))
+}
+
+lcrat_ct_form <- get_best_bic_formula(
+    case_at_next_screen
+    ~ logit1yrisk * . + 1,
+    bss_data,
+    "case_at_next_screen")
+lcrat_ct_form
+
+lcp_lcrat_ct_form <- update(lcrat_ct_form, ~ . + max_lcp_score)
+
+# Model setup ----
 logit_mod <- logistic_reg(mode = "classification") %>% set_engine("glm")
+
+# Refit to all data to get one model & get summary for each model
+# TODO 1. Send model summary tables for all three models (Table 1 for paper)
+
+# Train models ----
+    # 1. LCP
+lcp <- logit_mod %>% fit(lcp_form, data = data)
+lcp
+tidy(lcp)
+    # 2. LCRAT+CT (including nodule features) main effects & interactions
+lcrat_ct <- logit_mod %>% fit(lcrat_ct_form, data = data)
+lcrat_ct
+tidy(lcrat_ct)
+    # 3. LCP+LCRAT
+lcp_lcrat <- logit_mod %>% fit(lcp_lcrat_form, data = data)
+lcp_lcrat
+tidy(lcp_lcrat)
+    # 4. LCP+LCRAT+CT (including nodule features) main effects & interactions
+lcp_lcrat_ct <- logit_mod %>% fit(lcp_lcrat_ct_form, data = data)
+lcp_lcrat_ct
+tidy(lcp_lcrat_ct)
+
+# Predict ----
+    # 1. LCP
+lcp %>% parsnip::predict.model_fit(data = data)
+tidy(lcp)
+    # 2. LCRAT+CT (including nodule features) main effects & interactions
+lcrat_ct <- logit_mod %>% fit(lcrat_ct_form, data = data)
+lcrat_ct
+tidy(lcrat_ct)
+    # 3. LCP+LCRAT
+lcp_lcrat <- logit_mod %>% fit(lcp_lcrat_form, data = data)
+lcp_lcrat
+tidy(lcp_lcrat)
+    # 4. LCP+LCRAT+CT (including nodule features) main effects & interactions
+lcp_lcrat_ct <- logit_mod %>% fit(lcp_lcrat_ct_form, data = data)
+lcp_lcrat_ct
+tidy(lcp_lcrat_ct)
+
+
+
+# Cross-validation ----
 ctrl <- control_resamples(save_pred = TRUE)
 cv_train <- vfold_cv(data = data, v = 5, strata = "case_at_next_screen")
 
 # Fit and predict ----
-# Refit to all data to get one model & get summary for each model
-# TODO 1. Send model summary tables for all three models (Table 1 for paper)
-    # 1. LCP
-    # 2. LCRAT+CT (including nodule features) main effects & interactions
-    # 3. LCP+LCRAT
-    # 4. LCP+LCRAT+CT (including nodule features) main effects & interactions
 
 # Calculate sensitivity and get predictions 
 
